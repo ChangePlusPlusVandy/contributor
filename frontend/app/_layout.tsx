33156dc237e60d45b11a0ec73636048faea300ec
noahl <noahl25@lakesideschool.org>
2025-10-19 21:45:01 -0500

    load animation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,30 +1,66 @@
 import { NativeTabs, Icon, Label, Badge } from 'expo-router/unstable-native-tabs';
 import "../global.css"
+import { setBackgroundColorAsync } from 'expo-system-ui'
+import { useEffect, useState } from 'react';
+import Animated, { FadeOut } from 'react-native-reanimated';
+import StartupAnimation from '@/components/StartupAnimation';
+import { useFonts, Parisienne_400Regular } from "@expo-google-fonts/parisienne";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-	return (
-		<NativeTabs minimizeBehavior='automatic'>
-			<NativeTabs.Trigger name="(chat)">
-				<Label>Chat</Label>
-				<Icon sf={{ default: 'ellipses.bubble', selected: 'ellipses.bubble.fill' }} drawable="custom_android_drawable" />
-			</NativeTabs.Trigger>
-			<NativeTabs.Trigger name="(map)">
-				<Icon sf={{ default: 'map', selected: 'map.fill' }} drawable="custom_settings_drawable" />
-				<Label>Map</Label>
-			</NativeTabs.Trigger>
-			<NativeTabs.Trigger name="(home)">
-				<Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_settings_drawable" />
-				<Label>Home</Label>
-			</NativeTabs.Trigger>
-			<NativeTabs.Trigger name="(alerts)">
-				<Icon sf={{ default: 'bell', selected: 'bell.fill' }} drawable="custom_settings_drawable" />
-				<Label>Alerts</Label>
-				<Badge />
-			</NativeTabs.Trigger>
-			<NativeTabs.Trigger name="(more)">
-				<Icon sf={{ default: 'ellipsis', selected: 'ellipsis'}} drawable="custom_settings_drawable" />
-				<Label>More</Label>
-			</NativeTabs.Trigger>
-		</NativeTabs>
-	);
+
+	const [loaded, error] = useFonts({
+		Parisienne_400Regular,
+	});
+	const [animating, setAnimating] = useState<boolean>(true);
+
+	useEffect(() => {
+		setBackgroundColorAsync('#ffffff')
+	}, []);
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+
+	if (!loaded && !error) {
+		return null;
+	}
+	else {
+		return (
+			<>
+				{
+					animating && <StartupAnimation onFinish={() => setAnimating(false)} />
+				}
+				<NativeTabs minimizeBehavior='automatic' backgroundColor={"#ffffff"}>
+					<NativeTabs.Trigger name="(chat)">
+						<Label>Chat</Label>
+						<Icon sf={{ default: 'ellipses.bubble', selected: 'ellipses.bubble.fill' }} drawable="custom_android_drawable" />
+					</NativeTabs.Trigger>
+					<NativeTabs.Trigger name="(map)">
+						<Icon sf={{ default: 'map', selected: 'map.fill' }} drawable="custom_settings_drawable" />
+						<Label>Map</Label>
+					</NativeTabs.Trigger>
+					<NativeTabs.Trigger name="(home)">
+						<Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_settings_drawable" />
+						<Label>Home</Label>
+					</NativeTabs.Trigger>
+					<NativeTabs.Trigger name="(alerts)">
+						<Icon sf={{ default: 'bell', selected: 'bell.fill' }} drawable="custom_settings_drawable" />
+						<Label>Alerts</Label>
+						<Badge />
+					</NativeTabs.Trigger>
+					<NativeTabs.Trigger name="(more)">
+						<Icon sf={{ default: 'ellipsis', selected: 'ellipsis'}} drawable="custom_settings_drawable" />
+						<Label>More</Label>
+					</NativeTabs.Trigger>
+				</NativeTabs>
+			</>
+		);
+	}
 }
